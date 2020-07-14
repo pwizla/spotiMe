37b88adf5e2f3f4489633cc99e13f304d2ef2c81
pwizla <pwizla+github@gmail.com>
2020-07-14 17:09:01 +0200

    Add basic client with Spotify auth process

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="http://localhost:8888">Login to Spotify</a>
+    </div>
+  );
+}
+
+export default App;
